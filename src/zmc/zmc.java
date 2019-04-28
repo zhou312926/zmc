4875f49e82a858950755740714067c59744e3796
zhou312926 <mathew_zhou@163.com>
2019-04-28 21:30:41 +0800

    zmc

--- src/zmc/zmc.java
+++ src/zmc/zmc.java
@@ -3,5 +3,7 @@ package zmc;
 public class zmc {
     public static void main(String[] args) {
         System.out.println("zmc");
+        System.out.println("颜色是否变暗");
+        System.out.println("222222");
     }
 }
